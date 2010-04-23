ff6e8ba4a4d6a61455314148af6664766c2f5a88
ptspts <ptspts@66a892c4-edb7-11de-9298-fbd6a473cdd1>
2010-04-23 23:52:58 +0000

    added byte limit for nbfile.readline; added some tests for nbfile.readlines; fixed multiple bugs

---
+++ test/nbfile_test.py
@@ -0,0 +1,93 @@
+#! /usr/local/bin/stackless2.6
+# by pts@fazekas.hu at Sat Apr 24 00:25:31 CEST 2010
+
+import stackless
+import os
+import unittest
+
+from syncless import coio
+
+
+class NbfileTest(unittest.TestCase):
+  # TODO(pts): Write more tests.
+
+  def setUp(self):
+    try:
+      read_fd, write_fd = os.pipe()
+      self.f = coio.nbfile(read_fd, write_fd, write_buffer_limit=0,
+                           do_close=1)
+      read_fd = None
+    finally:
+      if read_fd is not None:  # Construction failed.
+        os.close(read_fd)
+        os.close(write_fd)
+
+  def tearDown(self):
+    self.f.close()
+
+  def testReadLine(self):
+    # TODO(pts): Add tests for the tasklet waiting in I/O.
+    self.assertEqual('', self.f.readline(0))
+    self.f.write('foobarbaz')
+    self.assertEqual('', self.f.readline(0))
+    self.assertEqual('fo', self.f.readline(2))
+    self.assertEqual('oba', self.f.readline(3))
+    self.f.write('X\n\nYZ\n')
+    self.assertEqual('r', self.f.readline(1))
+    self.assertEqual('', self.f.readline(0))
+    self.assertEqual('bazX\n', self.f.readline())
+    self.assertEqual('', self.f.readline(0))
+    self.assertEqual('\n', self.f.readline())
+    self.assertEqual('YZ\n', self.f.readline())
+    self.f.write('\nABC')
+    self.assertEqual('\n', self.f.readline(1))
+    self.assertEqual('ABC', self.f.readline(3))
+
+  def AssertReadLineWait(self, expected_read, to_write, limit=-1):
+    appender_calls = []
+    reader_tasklet = stackless.current
+    def Appender():
+      appender_calls.append(10)
+      self.f.write(to_write)
+      if to_write and limit:
+        appender_calls.append(20)
+        stackless.schedule()
+      else:
+        appender_calls.append(20)
+      if reader_tasklet.scheduled and not reader_tasklet.blocked:
+        appender_calls.append(30)
+      else:
+        appender_calls.append(31)
+        self.f.write('\n')
+    stackless.tasklet(Appender)()
+    if to_write and limit:
+      expected_calls = [10, 20]
+    else:
+      expected_calls = []
+    self.assertEqual([expected_read, expected_calls],
+                     [self.f.readline(limit), appender_calls])
+    stackless.schedule()
+    self.assertEqual([10, 20, 30], appender_calls)
+
+  def testReadLineWait(self):
+    self.AssertReadLineWait('foo', 'foo\nbar\nbaz\n', 3)
+    self.AssertReadLineWait('\n', '')
+    self.AssertReadLineWait('bar\n', '')
+    self.AssertReadLineWait('baz\n', '')
+    self.AssertReadLineWait('', '', 0)
+    self.AssertReadLineWait('', 'foo', 0)
+    self.AssertReadLineWait('foo', '', 3)
+
+  def testReadLineLongLine(self):
+    # Almost 1 MB. Since Linux usually sends at most 64 kB over a pipe at a
+    # time, sending and receiving 1 MB needs multiple EAGAIN for write(2),
+    # thus doing a complex tasklet and libevent interaction.
+    ks = 'ABCDEFGHI' * 111111
+    self.AssertReadLineWait(ks, ks, len(ks))
+    self.AssertReadLineWait('\n', '\n')
+    ksn = ks + '\n'
+    self.AssertReadLineWait(ksn, ksn + 'foo')
+    self.AssertReadLineWait('foo', '', 3)
+
+if __name__ == '__main__':
+  unittest.main()
